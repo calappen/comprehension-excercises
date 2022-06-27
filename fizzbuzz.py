53c1a8a02a85bb6ab73382153a4cabf3eca33970
calappen <chrislappen@gmail.com>
2022-06-27 10:14:58 -0700

    solution final final plus ultra

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,4 @@
-for i in range(101):
+for i in range(1,101):
     if i % 3 == 0 and i % 5 == 0:
         print("FizzBuzz")
     elif i % 3 == 0 and not i % 5 == 0:
@@ -7,3 +7,8 @@ for i in range(101):
         print("Buzz")
     else:
         print(i)
+print("-----------------------")
+fizzbuzz = ["FizzBuzz" if i % 3 == 0 and i % 5 == 0 else("Fizz" if i % 3 == 0 and not i % 5 == 0 else("Buzz" if i % 5 == 0 and not i % 3 == 0 else i)) for i in range(1,101)]
+
+for i in fizzbuzz:
+    print(i)
